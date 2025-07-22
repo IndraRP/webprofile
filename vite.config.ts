80ccd965f8133c779f32d6aa0d94929d1031491c
IndraRP <indraridho08@gmail.com>
2025-07-22 14:59:54 +0700

    PWA Set

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,40 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/webprofile/",
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "robots.txt"],
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
       manifest: {
-        name: "Web Profile",
-        short_name: "Profile",
-        description: "Profil PWA",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
+        name: "Your App Name",
+        short_name: "App",
         start_url: "/webprofile/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: "/webprofile/logoindra.png",
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/webprofile/logoindra.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
         ],
       },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024, // 6 MB
+      },
     }),
   ],
+  base: "/webprofile/",
 });
