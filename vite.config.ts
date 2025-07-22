7e90e005e096754a1963a2cf4422a85db42fdb5c
IndraRP <indraridho08@gmail.com>
2025-07-22 14:50:11 +0700

    penggunaan PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/webprofile/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "robots.txt"],
+      manifest: {
+        name: "Web Profile",
+        short_name: "Profile",
+        description: "Profil PWA",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/webprofile/",
+        icons: [
+          {
+            src: "/webprofile/logoindra.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/webprofile/logoindra.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
