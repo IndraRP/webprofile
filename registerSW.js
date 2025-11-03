d680b927b95b2c8fbc165603010253de68741491
IndraRP <indraridho08@gmail.com>
2025-11-03 10:59:03 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/webprofile/sw.js', { scope: '/webprofile/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
